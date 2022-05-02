709a48973a0c76bdf0e5b01c92521d44641d7985
jittel <nathan.c.ginn@gmail.com>
2022-05-02 13:31:12 -0700

    removed express, added package json

--- db/employee_db.sql
+++ db/employee_db.sql
@@ -4,20 +4,20 @@ CREATE database employee_db;
 USE employee_db;
 
 CREATE TABLE role (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL(10,2),
     department_id INT(10)
 );
 
 CREATE TABLE department (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     name VARCHAR(30),
     department_id INT(10)
 );
 
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT(10),
--- server.js
+++ server.js
@@ -1,13 +1,5 @@
-const express = require('express');
-// Import and require mysql2
+const inquirer = require('inquirer');
 const mysql = require('mysql2');
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection(
@@ -19,9 +11,5 @@ const db = mysql.createConnection(
     password: 'password',
     database: 'employee_db'
   },
-  console.log(`Connected to the books_db database.`)
+  console.log(`Connected to the employee_db database.`)
 );
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT} 🚀`);
-  });
